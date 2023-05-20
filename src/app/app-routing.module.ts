0879cc23f05174adcf8fbc9c44d2849b07281d46
ayushman76 <avenuedesign768@gmail.com>
2023-05-20 23:39:23 +0530

    fff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {
-    path:'', component: HomeComponent
-  }
+  {path:'', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
